7e0cd5c285737490c8ffc7cf0cf8512bc0aae169
ryo4004 <kato@ryo4004.net>
2023-02-13 20:01:12 +0900

    更新を反映

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-export const AUTH_API_URL = 'http://auth.winds-n.com'
-export const APP_API_URL = 'http://app.winds-n.com'
-export const SCORE_API_URL = 'http://score.winds-n.com'
-export const API_PATH = 'https://api.winds-n.com'
+export const AUTH_API_URL = process.env.REACT_APP_AUTH_API_URL as string
+export const APP_API_URL = process.env.REACT_APP_APP_API_URL as string
+export const SCORE_API_URL = process.env.REACT_APP_SCORE_API_URL as string
+export const API_PATH = process.env.REACT_APP_API_PATH as string
 export const VERSION = process.env.REACT_APP_VERSION as string
